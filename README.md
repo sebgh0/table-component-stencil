1135de0d13b044ac7ebd80f679a3675c3b4d1961
sebgh0 <108030090+sebgh0@users.noreply.github.com>
2024-08-13 23:10:00 -0500

    update README.md and minor changes

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
 # table-component-stencil
 Componente Web en Stencil para renderizar un api Rest hacia una tabla
+
+`MyTableComponentSasegovia1` es un componente web reutilizable desarrollado en Stencil que permite renderizar datos en una tabla HTML a partir de una API REST.
+
+## Características
+
+- **Carga de datos dinámicos:** Obtiene y muestra datos de una API proporcionada a través de la propiedad `apiUrl`.
+- **Interfaz responsiva:** Muestra los datos en una tabla HTML con encabezados dinámicos basados en las claves del JSON recibido.
+- **Manejo de errores:** Muestra mensajes de error si la solicitud falla o si los datos no son válidos.
+- **Indicador de carga:** Muestra un indicador de carga mientras se obtienen los datos.
+
+
+## Captura
+![alt text](<img/image.png>) 
+## Instalación
+### Alternativa 1:
+
+1. Clona este repositorio en tu máquina local:
+
+   ```bash
+   git clone https://github.com/sebgh0/table-component-stencil.git
+    ```
+
+2. Accede al directorio 
+    ```bash
+    cd component-01-sasegovia1
+    ```
+
+3. Instala las dependencias
+    ```bash
+    npm install
+    ``` 
+4. Inicia el servidor de desarrollo:
+    ```bash
+    npm start
+    ```
+
+### Alternativa 2:
+
+1. En tu proyecto, ejecuta el comando
+    ```bash
+    npm i componente-01-sasegovia
+    ```
+
+## USO
+- Una vez instalado y configurado, puedes utilizar tu componente en un proyecto HTML o en un framework que soporte componentes web.
+    ```bash
+        <my-table-component-sasegovia1 api-url="https://api.example.com/products"></my-table-component-sasegovia1>
+    ```
--- component-01-sasegovia/src/components.d.ts
+++ component-01-sasegovia/src/components.d.ts
@@ -6,31 +6,11 @@
  */
 import { HTMLStencilElement, JSXBase } from "@stencil/core/internal";
 export namespace Components {
-    interface MyComponent {
-        /**
-          * The first name
-         */
-        "first": string;
-        /**
-          * The last name
-         */
-        "last": string;
-        /**
-          * The middle name
-         */
-        "middle": string;
-    }
     interface MyTableComponentSasegovia1 {
         "apiUrl": string;
     }
 }
 declare global {
-    interface HTMLMyComponentElement extends Components.MyComponent, HTMLStencilElement {
-    }
-    var HTMLMyComponentElement: {
-        prototype: HTMLMyComponentElement;
-        new (): HTMLMyComponentElement;
-    };
     interface HTMLMyTableComponentSasegovia1Element extends Components.MyTableComponentSasegovia1, HTMLStencilElement {
     }
     var HTMLMyTableComponentSasegovia1Element: {
@@ -38,30 +18,14 @@ declare global {
         new (): HTMLMyTableComponentSasegovia1Element;
     };
     interface HTMLElementTagNameMap {
-        "my-component": HTMLMyComponentElement;
         "my-table-component-sasegovia1": HTMLMyTableComponentSasegovia1Element;
     }
 }
 declare namespace LocalJSX {
-    interface MyComponent {
-        /**
-          * The first name
-         */
-        "first"?: string;
-        /**
-          * The last name
-         */
-        "last"?: string;
-        /**
-          * The middle name
-         */
-        "middle"?: string;
-    }
     interface MyTableComponentSasegovia1 {
         "apiUrl"?: string;
     }
     interface IntrinsicElements {
-        "my-component": MyComponent;
         "my-table-component-sasegovia1": MyTableComponentSasegovia1;
     }
 }
@@ -69,7 +33,6 @@ export { LocalJSX as JSX };
 declare module "@stencil/core" {
     export namespace JSX {
         interface IntrinsicElements {
-            "my-component": LocalJSX.MyComponent & JSXBase.HTMLAttributes<HTMLMyComponentElement>;
             "my-table-component-sasegovia1": LocalJSX.MyTableComponentSasegovia1 & JSXBase.HTMLAttributes<HTMLMyTableComponentSasegovia1Element>;
         }
     }
